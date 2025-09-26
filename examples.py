770f4f5a480ce7bc3520e8566df52ded641e8cfa
jyoti-yadav28 <jyotiyadav282003@gmailcom>
2025-09-26 20:13:41 +0530

    should give an error

--- examples.py
+++ examples.py
@@ -1,4 +1,6 @@
 def main():
-    print("This is the sample github test")
+    # print("This is the sample github test")
+    x = 10 / 0   
+    print("This line will not run")
 if __name__ == "__main__":
     main()
